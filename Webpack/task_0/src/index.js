f4b9088e35864bd2757cb58b078ba98d82d90d81
Gabriel <fossecruor@gmail.com>
2025-02-07 10:36:43 +0100

    add files

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+
+$(document).ready(() => {
+    const b = $("body");
+    b.append("<p>Holberton Dashboard</p>");
+    b.append("<p>Dashboard data for the students</p>");
+    b.append("<p>Copyright - Holberton School</p>");
+});
