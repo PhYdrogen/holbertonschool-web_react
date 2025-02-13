d6bc5c2511fb31f1dbc443b283c86b16f9f3c31e
Gabriel <fossecruor@gmail.com>
2025-02-13 21:52:30 +0100

    IF ITS THE NEW LINE

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,8 @@
 import $ from "jquery";
 
 $(() => {
-    $("body").append("<p>Holberton Dashboard</p>");
-    $("body").append("<p>Dashboard data for the students</p>");
-    $("body").append("<p>Copyright - Holberton School</p>");
+    const b = $("body");
+    b.append("<p>Holberton Dashboard</p>");
+    b.append("<p>Dashboard data for the students</p>");
+    b.append("<p>Copyright - Holberton School</p>");
 });
