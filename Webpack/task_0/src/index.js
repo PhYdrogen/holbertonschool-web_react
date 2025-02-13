a963539d7fee8876e3704a72a930c502a65ca477
Gabriel <fossecruor@gmail.com>
2025-02-13 17:35:31 +0100

    update my fn

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 
-$(document).ready(() => {
+$(() => {
     const b = $("body");
     b.append("<p>Holberton Dashboard</p>");
     b.append("<p>Dashboard data for the students</p>");
