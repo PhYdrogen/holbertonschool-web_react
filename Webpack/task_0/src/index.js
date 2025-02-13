995cce72a4184ec5f7b2fb4df8ce4982c54d12c1
Gabriel <fossecruor@gmail.com>
2025-02-13 21:49:08 +0100

    modified

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,8 +1,7 @@
 import $ from "jquery";
 
 $(() => {
-    const b = $("body");
-    b.append("<p>Holberton Dashboard</p>");
-    b.append("<p>Dashboard data for the students</p>");
-    b.append("<p>Copyright - Holberton School</p>");
+    $("body").append("<p>Holberton Dashboard</p>");
+    $("body").append("<p>Dashboard data for the students</p>");
+    $("body").append("<p>Copyright - Holberton School</p>");
 });
