f7b1c4e66c9be2d09fd98636f40e090516ef4a9d
Gabriel <fossecruor@gmail.com>
2025-02-13 16:29:54 +0100

    added back the dashboard

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+import debounce from "lodash/debounce";
+
+let click = 0;
+const updateCounter = () => {
+    click = click + 1;
+        $('#count').text(click + ' clicks on the button');
+}
+
+$(document).ready(() => {
+    const b = $("body");
+    b.append("<p>Holberton Dashboard</p>");
+    b.append("<p>Dashboard data for the students</p>");
+    b.append("<button>Click here to get started</button>");
+    b.append("<p id='count'></p>");
+    b.append("<p>Copyright - Holberton School</p>");
+
+    $("button").click(debounce(updateCounter, 500));
+});
