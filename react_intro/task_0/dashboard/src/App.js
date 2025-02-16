ac2f93d74ee5a99dc7e3e3987b9183bbe525c7e4
Gabriel <fossecruor@gmail.com>
2025-02-16 20:00:09 +0100

    add code

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>School dashboard</h1>
+      </div>
+      <hr/>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <hr/>
+      <div className="App-footer">
+        <p>Copyright 2024 - holberton School</p>
+      </div>
     </div>
   );
 }
