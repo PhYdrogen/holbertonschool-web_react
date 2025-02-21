24e8da728c5f485cad36bead031ed3ac18b838e2
Gabriel <fossecruor@gmail.com>
2025-02-21 10:26:19 +0100

    update t2 because of t1

--- react_intro/task_2/dashboard/src/index.js
+++ react_intro/task_2/dashboard/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import Notifications from './Notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Notifications />
+    <App />
   </React.StrictMode>
 );
 
