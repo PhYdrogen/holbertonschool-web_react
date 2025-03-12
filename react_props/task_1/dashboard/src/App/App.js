0feb2fbea266b9c3b51196477a258656c0518b2e
Gabriel <fossecruor@gmail.com>
2025-03-12 11:45:35 +0100

    forgot notif

--- react_props/task_1/dashboard/src/App/App.js
+++ react_props/task_1/dashboard/src/App/App.js
@@ -1,10 +1,13 @@
 import { Header } from '../Header/Header';
 import { Login } from '../Login/Login';
 import { Footer } from '../Footer/Footer';
+import { Notification } from '../Notifications/Notifications';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Notification />
     <div className="App">
       <Header />
       <hr />
@@ -12,6 +15,7 @@ function App() {
       <hr />
       <Footer />
     </div>
+    </>
   );
 }
 
