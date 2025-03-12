515136fc4ae655a2001d3d241041993de2faede0
Gabriel <fossecruor@gmail.com>
2025-03-12 11:25:48 +0100

    All test in app

--- react_intro/task_3/dashboard/src/App.test.js
+++ react_intro/task_3/dashboard/src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Notifications from './Notifications';
+import Header from './Header';
+import Login from './Login';
+import Footer from './Footer';
 
 describe('App Component Tests', () => {
   it('renders without crashing', () => {
@@ -21,5 +25,25 @@ describe('App Component Tests', () => {
   it('renders a div with the class App-footer', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('.App-footer').exists()).toBe(true);
+  });
+
+  it('should contain the Notifications component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Notifications).length).toBe(1);
+  });
+
+  it('should contain the Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header).length).toBe(1);
+  });
+
+  it('should contain the Login component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Login).length).toBe(1);
+  });
+
+  it('should contain the Footer component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer).length).toBe(1);
   });
 });
