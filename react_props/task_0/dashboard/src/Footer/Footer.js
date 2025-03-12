62af3411cbb13bc05e67992ea0f231bb10d3d11e
Gabriel <fossecruor@gmail.com>
2025-03-12 09:36:38 +0100

    miss read

--- react_props/task_0/dashboard/src/Footer/Footer.js
+++ react_props/task_0/dashboard/src/Footer/Footer.js
@@ -1,29 +1,12 @@
-import logo from './logo.jpg';
-import './App.css';
+import './Footer.css';
 import { getFooterCopy } from '../utils/utils';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
-      <hr/>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label for="email">Email: </label>
-        <input id="email" type="text" />
-        <label for="password">Password: </label>
-        <input id="password" type="password"/>
-        <button>OK</button>
-      </div>
-      <hr/>
-      <div className="App-footer">
-        <p>{ getFooterCopy(true) }</p>
-      </div>
-    </div>
-  );
+function Footer() {
+    return (
+        <div className="App-footer">
+            <p>{getFooterCopy(true)}</p>
+        </div>
+    );
 }
 
-export default App;
+export default Footer;
--- react_props/task_0/dashboard/src/Header/Header.js
+++ react_props/task_0/dashboard/src/Header/Header.js
@@ -1,29 +1,13 @@
 import logo from './logo.jpg';
-import './App.css';
-import { getFooterCopy } from '../utils/utils';
+import './Header.css';
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
-      <hr/>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label for="email">Email: </label>
-        <input id="email" type="text" />
-        <label for="password">Password: </label>
-        <input id="password" type="password"/>
-        <button>OK</button>
-      </div>
-      <hr/>
-      <div className="App-footer">
-        <p>{ getFooterCopy(true) }</p>
-      </div>
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>School dashboard</h1>
     </div>
   );
 }
 
-export default App;
+export default Header;
