982c325947406370050d69931babc64412a2f9ef
Gabriel <fossecruor@gmail.com>
2025-03-25 15:06:49 +0100

    cp

--- react_component/task_2/dashboard/src/App/App.jsx
+++ react_component/task_2/dashboard/src/App/App.jsx
@@ -1,48 +1,80 @@
-import React, { Component } from "react";
-import Notifications from "../Notifications/Notifications";
-import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
-import "./App.css";
-import CourseList from "../CourseList/CourseList";
+import { Component } from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+import PropTypes from 'prop-types';
+
+const notificationsList = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+];
+
+const coursesList = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 }
+];
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.lastKey = "";
   }
 
+  handleLogout = () => {
+    console.log('Logging out from parent!');
+  };
+
   componentDidMount() {
-    document.body.addEventListener("keyup", (_) => {
-      this.lastKey = "";
-    });
-    document.body.addEventListener("keydown", (ev) => {
-      if (this.lastKey == 'Control' && ev.key == "h") {
+    document.addEventListener('keydown', this.handleKeydown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  handleKeydown = (e) => {
+    if (e.ctrlKey && e.key === "h") {
+      alert("Logging you out");
+      if (this.props.logOut) {
         this.props.logOut();
       }
-      this.lastKey = ev.key;
-    });
+    }
   }
 
   render() {
+    const { isLoggedIn = false } = this.props;
     return (
       <>
-        <div className="App">
-          <div className="App-head">
-            <Header />
-            <Notifications displayDrawer={this.props.displayDrawer} />
-          </div>
-
-          <div className="App-body border">
-            {this.props.isLoggedIn === false ? <Login /> : <CourseList />}
-          </div>
-          <div className="App-footer border">
-            <Footer />
-          </div>
-        </div>
+        <Notifications notifications={notificationsList} />
+        <>
+          <Header />
+          {
+            !isLoggedIn ? (
+              <Login />
+            ) : (
+              <CourseList courses={coursesList} />
+            )
+          }
+        </>
+        <Footer />
       </>
     );
   }
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => { },
+};
 
 export default App;
